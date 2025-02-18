44a575f4a72ce8f7bc753f586e88353707539af9
Katsiy <131786428+Katsiy@users.noreply.github.com>
2025-02-18 17:14:23 +0800

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,23 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { docsCollection } from './src/content.config';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "astro",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  stackbitVersion: '\~0.6.0',
+  ssgName: 'astro',
+  nodeVersion: '16',
+  contentSources: [
+    {
+      type: 'astro-content',
+      rootDir: './src/content',
+      collections: [docsCollection] // 直接引用你的 content.config.ts 中的集合
+    }
+  ],
+  modelExtensions: [
+    {
+      name: 'page',
+      type: 'page',
+      urlPath: '/{slug}',
+      filePath: 'src/content/docs/{slug}.md'
+    }
+  ]
+});
